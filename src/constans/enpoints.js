6b31b90af9a697e7a256914e360a0b4805a3c5c4
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-02 12:11:49 +0530

    endpoints added

---
+++ src/constans/enpoints.js
@@ -0,0 +1,10 @@
+
+export const AUTH_BASE ='/auth'
+export const AUTH_BASE_ADMIN ='/admin/auth'
+
+export const LOGIN = '/login'
+export const SIGNUP = '/signup'
+
+export const ADMIN_REDIRECT = '/admin/auth/login'
+export const USER_REDIRECT = '/auth/login'
+
