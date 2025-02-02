ffb6f669e5d3af63829676234a87ae583d29ee75
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-02 16:23:14 +0530

    user home attached

--- src/controllers/user/home/index.js
+++ src/controllers/user/home/index.js
@@ -1,10 +1,12 @@
 import { USER_HOME_PAGE } from "../../../constans/pages.js";
+import User from "../../../models/userSchema.js";
 
-export function homePage (req,res){
+export async function homePage (req,res){
     try{
-     return   res.status(200).send('Home page working')
+      const userId = req.session.userId
+      const getUserDetails = await User.findById(userId)
+     return   res.status(200).render(USER_HOME_PAGE,{user:getUserDetails})
     }catch(error){
-      return  res.status(500).send('Internal Server Error')
+      return  res.status(500).render(USER_HOME_PAGE,{user:{}})
     }
-    // res.status(200).render(USER_HOME_PAGE)
 }
