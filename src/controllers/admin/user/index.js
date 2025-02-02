ec662662b9fad811abbd3541217a94397bacae99
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-02 17:51:18 +0530

    delete user added

--- src/controllers/admin/user/index.js
+++ src/controllers/admin/user/index.js
@@ -2,58 +2,74 @@ import { ADMIN_USER_PAGE } from "../../../constans/pages.js"
 import User from "../../../models/userSchema.js"
 
 
-export const searchUser = async(req,res)=>{
-    try{
-        const {searchString} = req.query;
-        
-        let page =1
+export const searchUser = async (req, res) => {
+    try {
+        const { searchString } = req.query;
+
+        let page = 1
         const limit = 2;
-        const skip = (page - 1 ) * limit
+        const skip = (page - 1) * limit
 
         const totalUsersCount = await User.countDocuments()
-        const totalPages = (totalUsersCount/limit)
+        const totalPages = (totalUsersCount / limit)
 
-        const users = await User.find({isAdmin:false,
+        const users = await User.find({
+            isAdmin: false,
             $or: [
                 { email: { $regex: searchString, $options: "i" } },
                 { name: { $regex: searchString, $options: "i" } },
-              ],
+            ],
         }).skip(skip).limit(limit)
 
-        return res.status(200).render(ADMIN_USER_PAGE,{data:users,totalPages,currentPage:page})
-    }catch(error){
-        return res.status(500).render(ADMIN_USER_PAGE,{data:[],totalPages:0,currentPage:0})
+        return res.status(200).render(ADMIN_USER_PAGE, { data: users, totalPages, currentPage: page })
+    } catch (error) {
+        return res.status(500).render(ADMIN_USER_PAGE, { data: [], totalPages: 0, currentPage: 0 })
     }
 }
 
-export const userPage =async(req,res)=>{
-    try{
-        let {page} = req.query
+export const userPage = async (req, res) => {
+    try {
+        let { page } = req.query
 
-        page = parseInt(page)||1
+        page = parseInt(page) || 1
         const limit = 2
-        const skip = (page -1) *limit
+        const skip = (page - 1) * limit
 
         const totalUsersCount = await User.countDocuments()
-        const users = await User.find({isAdmin:false}).skip(skip).limit(limit)
+        const users = await User.find({ isAdmin: false }).skip(skip).limit(limit)
 
-        const totalPages = (totalUsersCount/limit)
+        const totalPages = (totalUsersCount / limit)
 
-       return res.status(200).render(ADMIN_USER_PAGE,{data:users,totalPages,currentPage:page})
+        return res.status(200).render(ADMIN_USER_PAGE, { data: users, totalPages, currentPage: page })
 
-    }catch(error){
+    } catch (error) {
         res.status(500).send("Internal Server Error")
     }
 }
 
-export const createUserPage =async () =>{}
+export const createUserPage = async () => { }
 
-export const createUser = async (req,res)=>{}
+export const createUser = async (req, res) => { }
 
-export const blockUser = async(req,res)=>{}
+export const blockUser = async (req, res) => { }
 
-export const updateUserPage = async ()=>{}
+export const updateUserPage = async () => { }
 
-export const updateUser = async () =>{}
+export const updateUser = async () => { }
 
-export const deleteUser = async () =>{}
+export const deleteUser = async (req, res) => {
+    try {
+        console.log( req.query)
+        console.log(req.params)
+        const id = req.params.id;
+        
+        await User.deleteOne({ _id: id });
+
+        res.redirect("/admin/users");
+    } catch (error) {
+        console.log(error.message);
+
+        res.status(500).send("Internal Server Error");
+
+    }
+}
--- src/routes/Admin/User/index.js
+++ src/routes/Admin/User/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { BASE_URL, SEARCH } from "../../../constans/enpoints.js";
-import { searchUser, userPage } from "../../../controllers/admin/user/index.js";
+import { BASE_URL, DYNAMIC_ID, SEARCH } from "../../../constans/enpoints.js";
+import { deleteUser, searchUser, userPage } from "../../../controllers/admin/user/index.js";
 
 const route = express.Router()
 
 route.get(BASE_URL,userPage)
 route.get(SEARCH,searchUser)
+route.get(DYNAMIC_ID,deleteUser)
 
 export default route
