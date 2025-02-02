782a3b90e345071f57eb3dee52361f4367a8d09b
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-02 13:32:01 +0530

    user signup functinality added

--- src/controllers/user/auth/index.js
+++ src/controllers/user/auth/index.js
@@ -1,17 +1,48 @@
+import { AUTH_BASE, LOGIN } from "../../../constans/enpoints.js";
 import { USER_LOGIN_PAGE, USER_SIGNUP_PAGE } from "../../../constans/pages.js"
 
-export async function loginPage(req,res){
+import User from '../../../models/userSchema.js'
+
+
+export async function loginPage(req, res) {
     res.status(200).render(USER_LOGIN_PAGE)
 }
 
-export async function loginUser(req,res){
+export async function loginUser(req, res) {
     res.status(200).send('working')
+
 }
 
-export async function signupPage(req,res){
-    res.status(200).render(USER_SIGNUP_PAGE)
+export async function signupPage(req, res) {
+    res.status(200).render(USER_SIGNUP_PAGE, { message: "", redirectUrl: '' })
 }
 
-export async function signupUser(req,res){
-    res.status(200).send('working')
+export async function signupUser(req, res) {
+    try {
+        const { fullName, email, password, confirmPassword } = req.body;
+
+        const findUser = await User.findOne({ email: email });
+
+        if (findUser) {
+            return res.status(409).render(USER_SIGNUP_PAGE, { message: "A User with this email already exists", redirectUrl: '' })
+        }
+        
+        if (password !== confirmPassword) {
+            return res.status(400).render(USER_SIGNUP_PAGE, { message: "Password do not match", redirectUrl: '' })
+        }
+        const userDetails = {
+            name: fullName,
+            email: email,
+            password,
+            isAdmin: true
+        }
+
+        const newUser = new User(userDetails)
+
+        await newUser.save()
+        return res.status(200).render(USER_SIGNUP_PAGE, { message: 'Your registration is successfull. Please login with your email and password', redirectUrl: AUTH_BASE+LOGIN })
+    }
+    catch (error) {
+        res.status(500).render(USER_SIGNUP_PAGE, { message: "Internal Server Error", redirectUrl: '' })
+    }
 }
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose;
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 
-const userSchema = new Schema({
+const userSchema = mongoose.Schema({
     name : {
         type: String,
         required : true
@@ -44,5 +44,21 @@ const userSchema = new Schema({
     timestamps:true
 })
 
+userSchema.methods.comparePassword = async function (plainPassword) {
+    return await bcrypt.compare(plainPassword, this.password);
+  };
+
+userSchema.methods.hashPassword = async function (plainPassword) {
+    const saltRounds = 10;
+    return await bcrypt.hash(plainPassword, saltRounds);
+  };
+
+  userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+      this.password = await this.hashPassword(this.password);
+    }
+    next();
+  });
+
 const User = mongoose.model("User", userSchema);
-module.exports = User;
+export default User
